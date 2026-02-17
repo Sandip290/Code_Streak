7568aaf12daefaa89fbaf8b17c0fa1d19e83827c
Sandip <sandipchakraborty165@gmail.com>
2026-02-18 01:07:36 +0530

    questions on Next Greater Number 1

---
+++ stacks_queues/NextGreater.java
@@ -0,0 +1,24 @@
+package stacks_queues;
+import java.util.Stack;
+
+class NextGreater {
+    public int[] nextGreaterElements(int[] nums) {
+        int[] nge = new int[nums.length];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 2 * nums.length - 1; i >= 0; i--) {
+            int current = nums[i % nums.length];
+            while (!st.empty() && st.peek() <= current) {
+                st.pop();
+            }
+            if (i < nums.length) {
+                if (st.empty()) {
+                    nge[i] = -1;
+                } else {
+                }
+            }
+            st.push(current);
+        }
+
+        return nge;
+    }
+}
